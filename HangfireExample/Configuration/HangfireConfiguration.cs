1bad1c6a0828e6da04c8721d66c52cb7deb55c5d
Alexandre Ito <alexandreh.ito@gmail.com>
2019-04-07 19:09:03 -0300

    First commit

---
+++ HangfireExample/Configuration/HangfireConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Hangfire;
+using Hangfire.MySql.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Data;
+
+namespace HangfireExample.Configuration
+{
+    public static class HangfireConfiguration
+    {
+        public static void AddHangfire(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("HangfireConnection");
+
+            services.AddHangfire(c => c
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseStorage(
+                    new MySqlStorage(connectionString,
+                        new MySqlStorageOptions()
+                        {
+                            TransactionIsolationLevel = IsolationLevel.ReadCommitted,
+                            QueuePollInterval = TimeSpan.FromSeconds(15),
+                            JobExpirationCheckInterval = TimeSpan.FromHours(1),
+                            CountersAggregateInterval = TimeSpan.FromMinutes(5),
+                            PrepareSchemaIfNecessary = true,
+                            DashboardJobListLimit = 50000,
+                            TransactionTimeout = TimeSpan.FromMinutes(1)
+                        })
+                ));
+
+            services.AddHangfireServer();
+        }
+
+
+        public static void UseHangfire(this IApplicationBuilder app)
+        {
+            app.UseHangfireDashboard();
+        }
+    }
+}
---
+++ HangfireExample/Pages/Index.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HangfireExample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public static List<JobHistory> jobs = new List<JobHistory>();
+
+        public void OnGet()
+        {
+            if (!string.IsNullOrEmpty(Request.Query["option"]))
+            {
+                var job = new JobHistory();
+                job.Id = Guid.NewGuid();
+                job.ScheduledAt = DateTime.Now;
+                job.Name = Request.Query["option"];
+                jobs.Add(job);
+
+                switch (Request.Query["option"])
+                {
+                    case "fire-forget":
+                        BackgroundJob.Enqueue(() => RunJob(job.Id));
+                        break;
+                    case "delayed":
+                        BackgroundJob.Schedule(() => RunJob(job.Id), TimeSpan.FromSeconds(30));
+                        break;
+                    case "recurring":
+                        RecurringJob.AddOrUpdate(() => RunJob(job.Id), "* * * * *");
+                        break;
+                    default: 
+                        // not found
+                        jobs.Remove(job);
+                        break;
+                }
+            }
+        }
+
+        public static void RunJob(Guid id)
+        {
+            var job = jobs.FirstOrDefault(p => p.Id == id);
+
+            if (job != null)
+            {
+                job.RunAt = DateTime.Now;
+                System.Diagnostics.Debug.WriteLine(job);
+            }
+        }
+    }
+
+    public class JobHistory
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ScheduledAt { get; set; }
+        public DateTime RunAt { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} | {1}: [{2} - {3}]", 
+                this.ScheduledAt.ToString("yyy-MM-dd HH:mm:ss"),
+                this.RunAt.ToString("yyy-MM-dd HH:mm:ss"),
+                this.Id,
+                this.Name);
+        }
+    }
+}
